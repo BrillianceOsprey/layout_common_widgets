a4d258b6edb3b0de9a5a6f12ab0bc90401d73c0d
Myat Soe <mssoelay91@gmail.com>
2023-10-16 14:45:34 +0630

    firebase set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
-// import 'package:flutter/material.dart';
-// import 'package:layout_testing/main_page.dart';
+import 'package:flutter/material.dart';
+import 'package:layout_testing/main_page.dart';
 
-// void main() => runApp(const MyApp());
+void main() => runApp(const MyApp());
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Layouts Demo',
-//       debugShowCheckedModeBanner: false,
-//       theme: ThemeData(
-//         primarySwatch: Colors.orange,
-//       ),
-//       home: const MainPage(),
-//     );
-//   }
-// }
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Layouts Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
